7284d10e0093a076c4fda1f3df2f5717c6127da5
Lee Seung Jin <dltmdwls154@gmail.com>
2024-05-09 23:37:00 +0900

    pinia 설치

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["~/assets/css/tailwindcss.css"],
+  modules: ["@pinia/nuxt"],
   postcss: {
     plugins: {
       tailwindcss: {},
