9cfa30a6605edef29b1d2f25cf07be62ccd74600
Lee Seung Jin <dltmdwls154@gmail.com>
2024-06-17 23:32:32 +0900

    Test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   css: ["~/assets/css/tailwindcss.css"],
-  modules: ["@pinia/nuxt"],
+  modules: ["@pinia/nuxt", "@hebilicious/vue-query-nuxt"],
   pinia: {
     storesDirs: ["./stores/**"],
   },
--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,20 @@ definePageMeta({
   layout: "default",
 });
 </script>
+
+<script>
+export default {
+  setup() {
+    this.asyncData();
+    return {};
+  },
+  async asyncData() {
+    console.log("zxcvxzcv");
+    alert("xc,vmnbz");
+  },
+};
+</script>
+
 <template>
   <h1 class="bg-[#863939]">Home Page</h1>
   <div>sdfaskldf</div>
