bdb2bf0a9635082bc00b6adb0634b23c7c7975b2
Lee Seung Jin <dltmdwls154@gmail.com>
2024-06-20 00:33:36 +0900

    Test

--- pages/list/index.vue
+++ pages/list/index.vue
@@ -11,6 +11,8 @@ const queryClient = useQueryClient();
 const api_url = "https://app.asana.com/api/1.0";
 const token = config.public.NUXT_ENV_API_KEY;
 const members = useState("members", () => []);
+const sections = useState("sections", () => []);
+
 const { isLoading, isError, data, error } = useQuery({
   queryKey: ["projects"],
   queryFn: () =>
@@ -18,6 +20,26 @@ const { isLoading, isError, data, error } = useQuery({
       headers: { Authorization: `Bearer ${token}`, accept: "application/json", "Content-Type": "application/json" },
     }),
 });
+
+const getSections = async (gid) => {
+  const opt_pretty = "true";
+
+  await axios
+    .get(`${api_url}/projects/${gid}/sections?opt_pretty=${opt_pretty}`, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(({ data }) => {
+      console.log(data);
+      sections.value = data.data;
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
 
 const getMembers = async ({ gid }) => {
   if (!gid) return;
@@ -34,12 +56,12 @@ const getMembers = async ({ gid }) => {
     })
     .then(({ data }) => {
       members.value = data.data;
+      getSections(gid);
     })
     .catch((error) => {
       members.value = [];
     });
 };
-const memberss = useMutation(getMembers);
 
 const asana = useState("asana", () => []);
 
@@ -49,6 +71,40 @@ const getData = async ({ gid }) => {
 
   await axios
     .get(`${api_url}/projects/${gid}/tasks?opt_pretty=${opt_pretty}&opt_fields=${opt_fields}`, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(({ data }) => {
+      asana.value = data.data;
+      getSections(gid);
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
+};
+
+const getDetail = async ({ gid }) => {
+  const opt_fields = "name,due_on,start_on,completed,assignee.name";
+  const opt_pretty = "true";
+
+  const space = await axios
+    .get(`${api_url}/users/${gid}?opt_pretty=${opt_pretty}&opt_fields=photo,workspaces.name`, {
+      headers: {
+        accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(({ data }) => data)
+    .catch((error) => null);
+
+  if (!space) return;
+
+  await axios
+    .get(`${api_url}/tasks/${gid}?opt_pretty=${opt_pretty}&opt_fields=${opt_fields}`, {
       headers: {
         accept: "application/json",
         "Content-Type": "application/json",
@@ -67,25 +123,50 @@ const getData = async ({ gid }) => {
 <template>
   <div class="w-full h-auto bg-[gray]">
     <div class="flex flex-col max-w-[1280px] gap-[100px] mx-auto">
-      <div class="grid grid-cols-4 gap-[20px] max-w-[1280px] mx-auto">
-        <div
-          v-for="item in data?.data"
-          @click="getMembers(item)"
-          :key="item.gid"
-          class="w-[200px] h-[200px] flex flex-col rounded-[8px] border-white border-[1px]"
-        >
-          <div class="flex-1 bg-[#3d3d3d]"></div>
-          <div class="text-center py-[10px]">{{ item.name }}</div>
+      <div>
+        <div class="text-[32px] py-[20px]">Projects</div>
+        <div class="grid grid-cols-[repeat(auto-fill,minmax(200px,_1fr))] gap-[20px] max-w-[1280px] mx-auto">
+          <div
+            v-for="item in data?.data"
+            @click="getMembers(item)"
+            :key="item.gid"
+            class="w-[200px] h-[200px] flex flex-col rounded-[8px] border-white border-[1px]"
+          >
+            <div class="flex-1 bg-[#3d3d3d]"></div>
+            <div class="text-center py-[10px]">{{ item.name }}</div>
+          </div>
         </div>
       </div>
-
-      <div class="max-w-[1280px] w-full grid grid-flow-col gap-[40px]">
-        <div
-          class="w-[200px] h-[200px] flex flex-col rounded-[8px] border-white border-[1px] bg-[#3d3d3d]"
-          v-for="item in members?.members"
-          :key="item.gid"
-        >
-          {{ item.name }}
+      <div class="max-w-[1280px] w-full">
+        <div class="text-[32px] py-[20px]">Task</div>
+        <div class="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,_1fr))] justify-start gap-[20px]">
+          <div
+            class="w-[200px] h-[200px] flex items-center justify-center text-white rounded-[8px] border-white border-[1px] bg-[#3d3d3d]"
+            v-for="item in sections"
+            :key="item.gid"
+          >
+            {{ item.name }}
+          </div>
+        </div>
+      </div>
+      <div class="max-w-[1280px] w-full">
+        <div class="text-[32px] py-[20px]">Members</div>
+        <div class="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,_1fr))] justify-start gap-[20px]">
+          <div
+            class="w-[200px] h-[200px] flex items-center justify-center text-white rounded-[8px] border-white border-[1px] bg-[#3d3d3d]"
+            v-for="item in members?.members"
+            :key="item.gid"
+            @click="getDetail(item)"
+          >
+            {{ item.name }}
+          </div>
+          <div
+            @click="getData(members)"
+            class="w-[200px] h-[200px] flex items-center justify-center text-white rounded-[8px] border-white border-[1px] bg-[#3d3d3d]"
+            v-if="members?.members"
+          >
+            전체보기
+          </div>
         </div>
       </div>
       <div class="w-full h-auto min-h-[400px] flex flex-col gap-[40px] max-w-[1280px] mx-auto">
