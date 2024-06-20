2f0ff1b485a12d805efb15bbbbb2fabdc426636e
이승진 <dltmdwls154@gmail.com>
2024-06-20 13:32:22 +0900

    Test

--- pages/list/index.vue
+++ pages/list/index.vue
@@ -12,6 +12,7 @@ const api_url = "https://app.asana.com/api/1.0";
 const token = config.public.NUXT_ENV_API_KEY;
 const members = useState("members", () => []);
 const sections = useState("sections", () => []);
+const filter = useState("filter", () => []);
 
 const { isLoading, isError, data, error } = useQuery({
   queryKey: ["projects"],
@@ -33,7 +34,6 @@ const getSections = async (gid) => {
       },
     })
     .then(({ data }) => {
-      console.log(data);
       sections.value = data.data;
     })
     .catch((error) => {
@@ -45,7 +45,7 @@ const getMembers = async ({ gid }) => {
   if (!gid) return;
   const opt_fields = "members.name";
   const opt_pretty = "true";
-
+  await getData({ gid });
   return await axios
     .get(`${api_url}/projects/${gid}?opt_fields=${opt_fields}&opt_pretty=${opt_pretty}`, {
       headers: {
@@ -80,43 +80,54 @@ const getData = async ({ gid }) => {
     .then(({ data }) => {
       asana.value = data.data;
       getSections(gid);
+
+      filter.value = asana.value;
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
 };
-
+const getAll = async () => {
+  filter.value = asana.value;
+};
 const getDetail = async ({ gid }) => {
-  const opt_fields = "name,due_on,start_on,completed,assignee.name";
-  const opt_pretty = "true";
-
-  const space = await axios
-    .get(`${api_url}/users/${gid}?opt_pretty=${opt_pretty}&opt_fields=photo,workspaces.name`, {
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then(({ data }) => data)
-    .catch((error) => null);
-
-  if (!space) return;
-
-  await axios
-    .get(`${api_url}/tasks/${gid}?opt_pretty=${opt_pretty}&opt_fields=${opt_fields}`, {
-      headers: {
-        accept: "application/json",
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .then(({ data }) => {
-      asana.value = data.data;
-    })
-    .catch((error) => {
-      console.error("Error fetching data:", error);
+  try {
+    filter.value = asana.value.filter(({ assignee }) => {
+      return assignee?.gid === gid;
     });
+  } catch (error) {
+    console.error("Error in getDetail:", error);
+  }
+  // const opt_fields = "name,due_on,start_on,completed,assignee.name";
+  // const opt_pretty = "true";
+
+  // const space = await axios
+  //   .get(`${api_url}/users/${gid}?opt_pretty=${opt_pretty}&opt_fields=photo,workspaces.name`, {
+  //     headers: {
+  //       accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   })
+  //   .then(({ data }) => data)
+  //   .catch((error) => null);
+
+  // if (!space) return;
+
+  // await axios
+  //   .get(`${api_url}/tasks/${gid}?opt_pretty=${opt_pretty}&opt_fields=${opt_fields}`, {
+  //     headers: {
+  //       accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       Authorization: `Bearer ${token}`,
+  //     },
+  //   })
+  //   .then(({ data }) => {
+  //     asana.value = data.data;
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error fetching data:", error);
+  //   });
 };
 </script>
 
@@ -161,7 +172,7 @@ const getDetail = async ({ gid }) => {
             {{ item.name }}
           </div>
           <div
-            @click="getData(members)"
+            @click="getAll"
             class="w-[200px] h-[200px] flex items-center justify-center text-white rounded-[8px] border-white border-[1px] bg-[#3d3d3d]"
             v-if="members?.members"
           >
@@ -170,7 +181,9 @@ const getDetail = async ({ gid }) => {
         </div>
       </div>
       <div class="w-full h-auto min-h-[400px] flex flex-col gap-[40px] max-w-[1280px] mx-auto">
-        <TaskListItem v-for="item in asana" :key="item.gid" :task="item" />
+        <div v-for="item in filter" :key="item.gid">
+          <TaskListItem :task="item" />
+        </div>
       </div>
     </div>
   </div>
