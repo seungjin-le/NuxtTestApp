233ddbc6b5feb09879dac069be67c341945de168
Lee Seung Jin <dltmdwls154@gmail.com>
2024-06-27 23:45:26 +0900

    Test

---
+++ pages/list/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import axios from "axios";
+definePageMeta({
+  title: "Asana test",
+  layout: "default",
+});
+const queryClient = useQueryClient();
+
+const { data, error, refetch } = useQuery({
+  queryKey: ["projects"],
+  queryFn: () => $fetch(`/api/v1?path=projects&name=asana`),
+});
+
+console.log(data.data);
+</script>
+
+<template>
+  <div @click="refetch()">asdf</div>
+</template>
---
+++ server/api/v1/index.get.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const token = config.public.NUXT_ENV_API_KEY;
+
+  return await axios
+    .get("https://app.asana.com/api/1.0/projects", {
+      headers: { Authorization: `Bearer ${token}`, accept: "application/json", "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      console.log(response.data);
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
+});
