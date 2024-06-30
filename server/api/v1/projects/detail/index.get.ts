f0eb64a238bae8a62503fe05f86aef8a56422d80
Lee Seung Jin <dltmdwls154@gmail.com>
2024-06-30 23:48:50 +0900

    Test

--- server/api/v1/projects/detail/index.get.ts
+++ server/api/v1/projects/detail/index.get.ts
@@ -6,6 +6,7 @@ export default defineEventHandler(async (event) => {
   const api_url = "https://app.asana.com/api/1.0";
   const opt_fields = "members.name";
   const opt_pretty = "true";
+  console.log(event);
   return await axios
     .get(`${api_url}/projects`, {
       headers: { Authorization: `Bearer ${token}`, accept: "application/json", "Content-Type": "application/json" },
