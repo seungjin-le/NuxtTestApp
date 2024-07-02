ef55f0ada62232da2ffa094271939fc2480f062a
Lee Seung Jin <dltmdwls154@gmail.com>
2024-07-02 23:41:09 +0900

    Test

---
+++ server/api/v1/projects/users/index.get.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default defineEventHandler(async (event) => {
+  const config = useRuntimeConfig();
+  const token = config.public.NUXT_ENV_API_KEY;
+  const api_url = "https://app.asana.com/api/1.0";
+  const opt_fields = "members.name";
+  const opt_pretty = "true";
+  console.log(event);
+  return await axios
+    .get(`${api_url}/projects?opt_fields=${opt_fields}`, {
+      headers: { Authorization: `Bearer ${token}`, accept: "application/json", "Content-Type": "application/json" },
+    })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      return error;
+    });
+});
