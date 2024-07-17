bf5a8fc1f1827416c6228f8803c0e3c6b7831940
Lee Seung Jin <dltmdwls154@gmail.com>
2024-07-17 23:55:48 +0900

    Test

--- components/containers/Header.vue
+++ components/containers/Header.vue
@@ -7,7 +7,7 @@ export default defineComponent({
 });
 </script>
 <template>
-  <div class="w-full py-[14px] flex items-center justify-center">
+  <div class="w-full py-[14px] flex items-center justify-center text-white">
     <h1 class="text-[24px] font-bold">Asana Task</h1>
   </div>
 </template>
