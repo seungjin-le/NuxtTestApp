4bd5ba391fb2c1c86630a6af9ecb1e40d9255b13
Lee Seung Jin <dltmdwls154@gmail.com>
2024-08-10 23:50:28 +0900

    Test

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@ const Section = defineAsyncComponent(() => import("~/components/containers/Secti
 </script>
 
 <template>
-  <div class="bg-[#2d2d2d] w-full">
+  <div class="bg-[#2d2d2d] w-full min-h-[100dvh]">
     <div class="w-full max-w-[1320px] h-full min-h-[100vh] flex flex-col items-center justify-between mx-auto">
       <Section>
         <Header />
