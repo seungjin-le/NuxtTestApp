28c83c726ab5beef25fa33b8c748becd2e6f596a
seungjin-le <dltmdwls154@gmail.com>
2024-11-13 16:16:56 +0900

    Test

--- pages/index.vue
+++ pages/index.vue
@@ -8,10 +8,75 @@ const jsondata = ref([]);
 const headers = ref([]);
 const file = ref(null);
 
+const datas = ref([
+  {
+    label: "메뉴 이름",
+    key: "menuName",
+    value: "menuName",
+    selected: false,
+  },
+  {
+    label: "1차 카테고리",
+    key: "category1",
+    value: "category1",
+    selected: false,
+  },
+  {
+    label: "2차 카테고리",
+    key: "category2",
+    value: "category2",
+    selected: false,
+  },
+
+  {
+    label: "메뉴 설명",
+    key: "menuDesc",
+    value: "menuDesc",
+    selected: false,
+  },
+  {
+    label: "메뉴 가격",
+    key: "menuPrice",
+    value: "menuPrice",
+    selected: false,
+  },
+  {
+    label: "메뉴 카테고리",
+    key: "menuCategory",
+    value: "menuCategory",
+    selected: false,
+  },
+  {
+    label: "메뉴 이미지",
+    key: "menuImage",
+    value: "menuImage",
+    selected: false,
+  },
+  {
+    label: "사용여부",
+    key: "use_status",
+    value: "use_status",
+    selected: false,
+  },
+  {
+    label: "성인인증",
+    key: "adult_auth",
+    value: "adult_auth",
+    selected: false,
+  },
+  {
+    label: "품절",
+    key: "sold_out",
+    value: "sold_out",
+    selected: false,
+  },
+]);
+
 const handleFileChange = (event) => {
   if (!event.target.files[0]) return;
   file.value = event.target.files[0];
   const reader = new FileReader();
+  const form = JSON.parse(JSON.stringify(datas.value));
   reader.onload = (e) => {
     const data = new Uint8Array(e.target.result);
     const workbook = XLSX.read(data, { type: "array" });
@@ -23,7 +88,7 @@ const handleFileChange = (event) => {
     jsondata.value.map((item) => {
       Object.keys(item).forEach((key) => (arr.includes(key) ? null : arr.push(key)));
     });
-    headers.value = arr.map((item) => ({ field: item, label: item }));
+    headers.value = arr.map((item) => ({ field: item, label: item, value: JSON.parse(JSON.stringify(form)) }));
 
     jsondata.value = jsondata.value.map((item) => {
       headers.value.forEach((header) => {
@@ -38,28 +103,95 @@ const handleFileChange = (event) => {
 
 const headerRefs = ref([]);
 
-const datas = ref([
+const firstCategory = [
   {
-    label: "메뉴 이름",
-    key: "menuName",
+    label: "1-1",
+    value: "category1_1",
+    selected: false,
   },
   {
-    label: "메뉴 설명",
-    key: "menuDesc",
+    label: "1-2",
+    value: "category1_2",
+    selected: false,
   },
   {
-    label: "메뉴 가격",
-    key: "menuPrice",
+    label: "1-3",
+    value: "category1_3",
+    selected: false,
   },
   {
-    label: "메뉴 카테고리",
-    key: "menuCategory",
+    label: "1-4",
+    value: "category1_4",
+    selected: false,
   },
   {
-    label: "메뉴 이미지",
-    key: "menuImage",
+    label: "1-5",
+    value: "category1_5",
+    selected: false,
   },
-]);
+
+  {
+    label: "1-6",
+    value: "category1_6",
+    selected: false,
+  },
+  {
+    label: "1-7",
+    value: "category1_7",
+    selected: false,
+  },
+];
+const secondCategory = [
+  {
+    label: "2-1",
+    value: "category2_1",
+    selected: false,
+  },
+  {
+    label: "2-2",
+    value: "category2_2",
+    selected: false,
+  },
+  {
+    label: "2-3",
+    value: "category2_3",
+    selected: false,
+  },
+  {
+    label: "2-4",
+    value: "category2_4",
+    selected: false,
+  },
+  {
+    label: "2-5",
+    value: "category2_5",
+    selected: false,
+  },
+  {
+    label: "2-6",
+    value: "category2_6",
+    selected: false,
+  },
+  {
+    label: "2-7",
+    value: "category2_7",
+    selected: false,
+  },
+];
+
+const handleSelectChange = (header, index, event) => {
+  header.value.find((item) => (item.selected = item.value === event.target.value));
+
+  jsondata.value.map((item) => {
+    if (event.target.value === "category1") {
+      item[header.field]["select"] = JSON.parse(JSON.stringify(firstCategory));
+    } else if (event.target.value === "category2") {
+      item[header.field]["select"] = JSON.parse(JSON.stringify(secondCategory));
+    } else {
+      item[header.field]["select"] = [];
+    }
+  });
+};
 </script>
 
 <template>
@@ -79,10 +211,14 @@ const datas = ref([
                 ref="headerRefs"
                 @dblclick="
                   () => {
-                    headers.map((item, i) => {
-                      item.sticky = i <= index;
-                      item.width = i === 0 ? 0 : i <= index ? headerRefs[i].offsetLeft + 1 : 0;
-                    });
+                    if (!headers[index].sticky) {
+                      headers.map((item, i) => {
+                        item.sticky = i <= index;
+                        item.width = i === 0 ? 0 : i <= index ? headerRefs[i].offsetLeft + 1 : 0;
+                      });
+                    } else {
+                      headers.map((item, i) => (item.sticky = false));
+                    }
                   }
                 "
                 :class="{
@@ -90,14 +226,53 @@ const datas = ref([
                 }"
               >
                 {{ header.label }}
+
+                <div class="relative" :style="{ zIndex: header.sticky ? 999 : 0 }">
+                  <select
+                    :name="`select_${index}`"
+                    :id="`select_${index}`"
+                    @change="(event) => handleSelectChange(header, index, event)"
+                    :style="{ zIndex: header.sticky ? 999 : 0 }"
+                  >
+                    <option v-for="item in header.value" :value="item.value" :key="`${item.label}_${index}`">
+                      {{ item.label }}
+                    </option>
+                  </select>
+                </div>
               </th>
             </tr>
           </thead>
 
           <tbody>
             <tr v-for="(row, index) in jsondata" :key="`rows_${index}`" class="relative">
-              <td v-for="(header, index) in headers" class="relative">
-                <Cell v-model="row[header.field]" />
+              <td
+                v-for="(header, index) in headers"
+                class="relative"
+                :style="{
+                  position: header.sticky ? 'sticky' : 'relative',
+                  left: header.sticky ? header.width + 'px' : 0,
+                  zIndex: header.sticky ? 999 : 0,
+                  background: header.sticky ? 'white' : 'transparent',
+                }"
+              >
+                <Cell
+                  v-if="
+                    header.value.find((item) => item.selected)?.value !== 'category2' &&
+                    header.value.find((item) => item.selected)?.value !== 'category1'
+                  "
+                  v-model="row[header.field]"
+                />
+                <div v-else :style="{ zIndex: header.sticky ? 999 : 0 }">
+                  <select>
+                    <option
+                      v-for="item in row[header.field]['select']"
+                      :value="item?.value"
+                      :key="`${item?.label}_${index}`"
+                    >
+                      {{ item?.label }}
+                    </option>
+                  </select>
+                </div>
               </td>
             </tr>
           </tbody>
